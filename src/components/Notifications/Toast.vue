635f3fa98e795e5a96154b890c1d731d6c248d87
Narayan <wovosoft@gmail.com>
2023-10-27 09:14:24 +0600

    Updated

--- src/components/Notifications/Toast.vue
+++ src/components/Notifications/Toast.vue
@@ -1,24 +1,24 @@
 <template>
-	<teleport :to="to">
-		<div
-			v-if="shouldGenerate"
-			ref="element"
-			v-bind="$attrs"
-			:class="classes"
-			role="alert"
-			aria-live="assertive"
-			aria-atomic="true">
-			<div class="toast-header" v-if="$slots.header || header">
-				<slot name="header">
-					<strong class="me-auto">{{ header }}</strong>
-				</slot>
-				<ButtonClose v-if="!noCloseButton" @click="visible = false"/>
-			</div>
-			<ToastBody v-if="!noBody">
-				<slot/>
-			</ToastBody>
-		</div>
-	</teleport>
+    <!--    <teleport :to="to">-->
+    <div
+        v-if="shouldGenerate"
+        ref="element"
+        v-bind="$attrs"
+        :class="classes"
+        role="alert"
+        aria-live="assertive"
+        aria-atomic="true">
+        <div class="toast-header" v-if="$slots.header || header">
+            <slot name="header">
+                <strong class="me-auto">{{header}}</strong>
+            </slot>
+            <ButtonClose v-if="!noCloseButton" @click="visible = false"/>
+        </div>
+        <ToastBody v-if="!noBody">
+            <slot/>
+        </ToastBody>
+    </div>
+    <!--    </teleport>-->
 </template>
 
 <script lang="ts" setup>
@@ -29,17 +29,18 @@ import {getTransitionDurationFromElement} from "@/composables/useHelpers";
 import {ToastProps} from "@/components/Notifications/index";
 
 const props = withDefaults(defineProps<ToastProps>(), {
-	fade: true,
-	timeout: 3,
+    fade: true,
+    timeout: 3,
+    to: 'body',//needs to modify so that it can be used without teleport as well
 });
 
 const emit = defineEmits<{
-	"update:show": [value: boolean];
-	"update:modelValue": [value: boolean];
-	"shown": [value: boolean];
-	"hidden": [value: boolean];
-	"showing": [value: boolean];
-	"hiding": [value: boolean];
+    "update:show": [value: boolean];
+    "update:modelValue": [value: boolean];
+    "shown": [value: boolean];
+    "hidden": [value: boolean];
+    "showing": [value: boolean];
+    "hiding": [value: boolean];
 }>();
 
 const visible = ref<boolean>(!!props.modelValue);
@@ -48,34 +49,34 @@ const shouldGenerate = computed(() => !props.lazy || (visible.value && props.laz
 
 //when show is defined
 if (props.show != null) {
-	visible.value = !!props.show;
+    visible.value = !!props.show;
 }
 
 watch(() => props.show, value => {
-	if (value !== visible.value) {
-		visible.value = !!value;
-	}
+    if (value !== visible.value) {
+        visible.value = !!value;
+    }
 });
 
 watch(() => props.modelValue, value => {
-	if (value !== visible.value) {
-		visible.value = !!value;
-	}
+    if (value !== visible.value) {
+        visible.value = !!value;
+    }
 });
 
 let timer: number | null = null;
 
 function setTimer() {
-	timer = setTimeout(() => {
-		visible.value = false;
-	}, Number(props.timeout) * 1000);
+    timer = setTimeout(() => {
+        visible.value = false;
+    }, Number(props.timeout) * 1000);
 }
 
 function clearTimer() {
-	if (timer) {
-		clearTimeout(timer);
-		timer = null;
-	}
+    if (timer) {
+        clearTimeout(timer);
+        timer = null;
+    }
 }
 
 onBeforeUnmount(clearTimer);
@@ -83,57 +84,57 @@ onBeforeUnmount(clearTimer);
 const element = ref<HTMLElement>();
 
 watch(visible, (value) => {
-	if (!element?.value) {
-		return;
-	}
-	
-	if (value) {
-		//when showing
-		// shouldGenerate.value=!props.lazy || (visible.value && props.lazy);
-		emit("showing", true);
-		showing.value = true;
-		setTimeout(() => {
-			showing.value = false;
-			emit("update:modelValue", true);
-			emit("update:show", true);
-			emit("shown", true);
-			
-			//when auto hide is disabled.
-			if (Number(props.timeout) <= 0 || !props.timeout) {
-				clearTimer();
-			} else {
-				setTimer();
-			}
-		}, getTransitionDurationFromElement(element.value));
-	} else {
-		emit("hiding", true);
-		showing.value = true;
-		setTimeout(() => {
-			showing.value = false;
-			emit("update:modelValue", false);
-			emit("update:show", false);
-			emit("hidden", true);
-			clearTimer();
-			//when hiding
-			// shouldGenerate.value=!props.lazy || (visible.value && props.lazy);
-		}, getTransitionDurationFromElement(element.value));
-	}
+    if (!element?.value) {
+        return;
+    }
+
+    if (value) {
+        //when showing
+        // shouldGenerate.value=!props.lazy || (visible.value && props.lazy);
+        emit("showing", true);
+        showing.value = true;
+        setTimeout(() => {
+            showing.value = false;
+            emit("update:modelValue", true);
+            emit("update:show", true);
+            emit("shown", true);
+
+            //when auto hide is disabled.
+            if (Number(props.timeout) <= 0 || !props.timeout) {
+                clearTimer();
+            } else {
+                setTimer();
+            }
+        }, getTransitionDurationFromElement(element.value));
+    } else {
+        emit("hiding", true);
+        showing.value = true;
+        setTimeout(() => {
+            showing.value = false;
+            emit("update:modelValue", false);
+            emit("update:show", false);
+            emit("hidden", true);
+            clearTimer();
+            //when hiding
+            // shouldGenerate.value=!props.lazy || (visible.value && props.lazy);
+        }, getTransitionDurationFromElement(element.value));
+    }
 });
 
 defineExpose({
-	show: () => (visible.value = true),
-	hide: () => (visible.value = false),
-	toggle: () => (visible.value = !visible.value),
+    show: () => (visible.value = true),
+    hide: () => (visible.value = false),
+    toggle: () => (visible.value = !visible.value),
 });
 
 const classes = computed(() => [
-	"toast",
-	{
-		show: visible.value,
-		hide: !visible.value,
-		showing: showing.value,
-		fade: props.fade,
-		["bg-" + props.variant]: props.variant,
-	},
+    "toast",
+    {
+        show: visible.value,
+        hide: !visible.value,
+        showing: showing.value,
+        fade: props.fade,
+        ["bg-" + props.variant]: props.variant,
+    },
 ]);
 </script>
--- src/pages/Notifications/Index.vue
+++ src/pages/Notifications/Index.vue
@@ -2,20 +2,35 @@
 
 import Button from "@/components/Buttons/Button.vue";
 import Popover from "@/components/Notifications/Popover.vue";
+import Toast from "@/components/Notifications/Toast.vue";
+import ToastContainer from "@/components/Notifications/ToastContainer.vue";
+import Tooltip from "@/components/Notifications/Tooltip.vue";
 </script>
 
 <template>
-	<h2 class="mt-3">Example</h2>
-	<Button id="p1">Popover Default</Button>
-	<Popover target="#p1" placement="right">
-		Hello Popover
-	</Popover>
-	
-	<h2 class="mt-3">Four Directions</h2>
-	<template v-for="placement in ['left','right','top','bottom']">
-		<Button :id="placement" class="me-3">Popover {{ placement }}</Button>
-		<Popover :target="'#'+placement" :placement="placement">
-			Hello Popover
-		</Popover>
-	</template>
+    <h2 class="mt-3">Example</h2>
+    <Button id="p1">Popover Default</Button>
+    <Popover target="#p1">
+        Hello Popover
+    </Popover>
+
+    <h2 class="mt-3">Four Directions</h2>
+    <template v-for="placement in ['left','right','top','bottom']">
+        <Button :id="placement" class="me-3">Popover {{placement}}</Button>
+        <Popover :target="'#'+placement" :placement="placement">
+            Hello Popover
+        </Popover>
+    </template>
+
+    <h1 class="mt-3">Default Toast</h1>
+    <Button @click="$refs.toast_def.toggle()">Toggle Default Toast</Button>
+    <ToastContainer placement="top-end" class="p-2">
+        <Toast ref="toast_def" header="Toast Header">
+            <p>Toast Body</p>
+        </Toast>
+    </ToastContainer>
+
+    <h1 class="mt-3">Tooltip</h1>
+    <Button id="tooltip_target">Click Me</Button>
+    <Tooltip target="tooltip_target">Tooltip Content</Tooltip>
 </template>
